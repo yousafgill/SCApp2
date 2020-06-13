97012ab9c3e488650014acfc88092ba19a120cb8
Yousaf Gill <yousafgill@gmail.com>
2020-06-14 02:20:40 +0500

    step2

--- ShoppingCartApp/ShoppingCartAppUnitTest/UnitTest1.cs
+++ ShoppingCartApp/ShoppingCartAppUnitTest/UnitTest1.cs
@@ -23,5 +23,31 @@ namespace ShoppingCartAppUnitTest
 
 			Assert.AreEqual(expected, actual);
 		}
+
+		[TestMethod]
+		public void AddFiveAndThreeProducts()
+		{
+
+			//Expected Result
+			decimal expected = (decimal)319.92;
+			ShoppingCartApp.ShoppingCart cart = new ShoppingCartApp.ShoppingCart();
+
+			// Add 5 Dove Soaps to Shopping Cart
+			for (int i = 0; i < 5; i++)
+			{
+				ShoppingCartApp.Product p = new ShoppingCartApp.Product("Dove Soap", (decimal)39.99);
+				cart.AddProduct(p, 1);
+			}
+
+			// Add 3 Dove Soaps to Shopping Cart
+			for (int i = 0; i < 3; i++)
+			{
+				ShoppingCartApp.Product p = new ShoppingCartApp.Product("Dove Soap", (decimal)39.99);
+				cart.AddProduct(p, 1);
+			}
+			//Actual Result after adding 8 Dove Soap of Unit Price 39.99
+			decimal actual = cart.TotalCartAmount();
+			Assert.AreEqual(expected, actual);
+		}
 	}
 }
